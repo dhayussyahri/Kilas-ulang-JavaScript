182867465a61e09a7c4c89316895ff4f86bee8a5
dhayus <74321000+dhayus@users.noreply.github.com>
2024-02-20 12:21:34 +0700

    24/02/20 : Belajar IF statement JavaScript

---
+++ script.js
@@ -0,0 +1,16 @@
+// console.log("sebelum pembanding")
+// if (1 + 1 === 2) {
+//   console.log("Perhitungan masih betul")
+// }
+// console.log("setelah pembanding")
+
+let angka = Math.random()
+console.log(angka)
+
+if (angka >= 0.5) {
+  console.log("Angka Lebih besar dari 0,5")
+}
+
+if (angka <= 0.5) {
+  console.log("Angka lebih kecil dari 0,5")
+}
