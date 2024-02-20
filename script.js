28d6423fc424ed9fb7c4cb82c6cff3e13b67d079
dhayus <74321000+dhayus@users.noreply.github.com>
2024-02-20 12:28:16 +0700

    24/02/20: Belajar if else statement JavaScript

--- script.js
+++ script.js
@@ -4,13 +4,22 @@
 // }
 // console.log("setelah pembanding")
 
-let angka = Math.random()
-console.log(angka)
+//iF STATEMENT
+// let angka = Math.random()
+// console.log(angka)
 
-if (angka >= 0.5) {
-  console.log("Angka Lebih besar dari 0,5")
-}
+// if (angka >= 0.5) {
+//   console.log("Angka Lebih besar dari 0,5")
+// }
 
-if (angka <= 0.5) {
-  console.log("Angka lebih kecil dari 0,5")
+// if (angka <= 0.5) {
+//   console.log("Angka lebih kecil dari 0,5")
+// }
+
+const iniHari = "Senin"
+
+if (iniHari === "Senin") {
+  console.log("Selamat Bekerja Kawan")
+} else if (iniHari === "Sabtu") {
+  console.log("Semoga jangan cepat berlalu")
 }
