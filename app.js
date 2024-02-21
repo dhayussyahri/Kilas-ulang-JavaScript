5251aafeeec54ffe4cd97a23f7da3766fb4b0978
dhayus <74321000+dhayus@users.noreply.github.com>
2024-02-21 11:19:30 +0700

    21/02/24 : Mendapatkan Data Dari Array Dengan Perulangan

--- app.js
+++ app.js
@@ -4,7 +4,13 @@
 // }
 
 // contoh lain dari perulangan for pengurangan hingga perkalian
-for (let i = 10; i <= 100000; i *= 10) {
- console.log('saya ada didalam loop');
- console.log(i);
+// for (let i = 10; i <= 100000; i *= 10) {
+//  console.log('saya ada didalam loop');
+//  console.log(i);
+// }
+
+const animals = ['cat', 'deer', 'hog', 'koala'];
+
+for (let i = 0; i <= animals.length; i++) {
+ console.log(i, animals[1]);
 }
