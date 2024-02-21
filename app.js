55bc5a44cc6687a0d04b53dc030b2ecc38642d62
dhayus <74321000+dhayus@users.noreply.github.com>
2024-02-21 11:36:00 +0700

    21/02/24 : Nasted Loop

--- app.js
+++ app.js
@@ -9,8 +9,18 @@
 //  console.log(i);
 // }
 
-const animals = ['cat', 'deer', 'hog', 'koala'];
+// Mendapatkan Data Dari Array Dengan Perulangan
+// const animals = ['cat', 'deer', 'hog', 'koala'];
 
-for (let i = 0; i <= animals.length; i++) {
- console.log(i, animals[1]);
+// for (let i = 0; i <= animals.length; i++) {
+//  console.log(i, animals[1]);
+// }
+
+// Nasted Loop
+const pilihan = 'abcd';
+for (let i = 1; i <= 10; i++) {
+ console.log(`${i}. Soal nomer ${i}:`)
+ for (let j = 0; j < pilihan.length; j++) {
+  console.log(`${pilihan[j]}. pilihan jawaban`);
+ }
 }
