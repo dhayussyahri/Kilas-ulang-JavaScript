7aa7806832d840c82f03f5dc72ae6138db6e4c4f
dhayus <74321000+dhayus@users.noreply.github.com>
2024-02-21 20:42:02 +0700

    21/02/24 : Cara Elegan Melakukan Perulangan Pada Array For Of

--- app2.js
+++ app2.js
@@ -1,23 +1,33 @@
 // Membuat Game Tebak Tebakkan Angka Dengan While
 
-let maximum = parseInt(prompt('Masukkan Nilai Maksimal'));
-while (!maximum) {
-  maximum = parseInt(prompt('Masukkan Nilai Maksimal !'));
+// let maximum = parseInt(prompt('Masukkan Nilai Maksimal'));
+// while (!maximum) {
+//   maximum = parseInt(prompt('Masukkan Nilai Maksimal !'));
+// }
+
+// const targetNum = Math.floor(Math.random() * maximum) + 1;
+// console.log(targetNum);
+
+// let guess = parseInt(prompt('Ini Tebakan pertama kamu !'));
+// let attempt = 1;
+
+// while (parseInt(guess) !== targetNum) {
+//   attempt++;
+//   if (guess > targetNum) {
+//     guess = prompt('Terlalu tinggi! Tebak lagi :');
+//   } else {
+//     guess = prompt('Terlalu rendah! Tebak lagi :');
+//   }
+// }
+
+// alert(`selamat tebakkan anda benar! Dengan Percobaan ${attempt} kali`);
+
+// *************************************************
+
+// Cara Elegan Melakukan Perulangan Pada Array For Of
+
+const fruits = ['Banana', 'Orange', 'Apple', 'Manggo'];
+
+for (let fruit of fruits) {
+  console.log(`Buah ${fruit}`);
 }
-
-const targetNum = Math.floor(Math.random() * maximum) + 1;
-console.log(targetNum);
-
-let guess = parseInt(prompt('Ini Tebakan pertama kamu !'));
-let attempt = 1;
-
-while (parseInt(guess) !== targetNum) {
-  attempt++;
-  if (guess > targetNum) {
-    guess = prompt('Terlalu tinggi! Tebak lagi :');
-  } else {
-    guess = prompt('Terlalu rendah! Tebak lagi :');
-  }
-}
-
-alert(`selamat tebakkan anda benar! Dengan Percobaan ${attempt} kali`);
