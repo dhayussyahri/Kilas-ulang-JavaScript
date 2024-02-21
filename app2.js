5f2ac87dfffba1bc099d4e7c533cf4e7ac6abc53
dhayus <74321000+dhayus@users.noreply.github.com>
2024-02-21 20:56:04 +0700

    21/02/24 : Melakukan Perulangan Pada Object Dengan Elegan For In

--- app2.js
+++ app2.js
@@ -26,8 +26,38 @@
 
 // Cara Elegan Melakukan Perulangan Pada Array For Of
 
-const fruits = ['Banana', 'Orange', 'Apple', 'Manggo'];
+// const fruits = ['Banana', 'Orange', 'Apple', 'Manggo'];
 
-for (let fruit of fruits) {
-  console.log(`Buah ${fruit}`);
+// for (let fruit of fruits) {
+//   console.log(`Buah ${fruit}`);
+// }
+
+// *************************************************
+
+// Melakukan Perulangan Pada Object Dengan Elegan For In
+
+const studentScore = {
+  Olivia: 29,
+  Liam: 13,
+  Emma: 40,
+  Noah: 18,
+  Amelia: 32,
+  Oliver: 10,
+  Ava: 11,
+  Elijah: 21,
+  Shopia: 14,
+  Mateo: 22,
+};
+
+// for (let student in studentScore) {
+//   console.log(`${student} memiliki Score ${studentScore[student]}`);
+// }
+
+let total = 0;
+let scores = Object.values(studentScore);
+for (let score of scores) {
+  total += score;
+
 }
+
+console.log(total / scores.length);
