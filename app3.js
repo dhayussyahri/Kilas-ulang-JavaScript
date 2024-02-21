14f998832dcd02940219753aa18f9a89ea507146
dhayus <74321000+dhayus@users.noreply.github.com>
2024-02-21 21:14:30 +0700

    21/02/24: Pengenalan Function Di Javascript

---
+++ app3.js
@@ -0,0 +1,12 @@
+// Materi FUNCTION DAN METHOD PADA JAVASCRIPT
+
+// Pengenalan Function Di Javascript
+
+// let LemparDadu = Math.floor(Math.random() * 6) + 1;
+// console.log(LemparDadu);
+
+function LemparDadu() {
+  console.log(Math.floor(Math.random() * 6) + 1);
+}
+
+// Panggil di console browser dengan LemparDadu()
