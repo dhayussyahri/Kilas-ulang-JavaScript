a295f5415917c91754da6428f6cb53a2690f07cd
dhayus <74321000+dhayus@users.noreply.github.com>
2024-02-21 11:06:31 +0700

    21/02/24: Contoh lain dari perulangan

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
-for (let i = 1; i <= 10; i++) {
+// Perulangan For JavaScript !
+// for (let i = 1; i <= 10; i++) {
+//  console.log(i);
+// }
+
+// contoh lain dari perulangan for pengurangan hingga perkalian
+for (let i = 10; i <= 100000; i *= 10) {
+ console.log('saya ada didalam loop');
  console.log(i);
 }
