7c6cbb3927766f2d5c6d54b1c5a80b6b68a90f57
dhayus <74321000+dhayus@users.noreply.github.com>
2024-02-23 13:57:29 +0700

    23/02/24: Arrow Function

--- app4.js
+++ app4.js
@@ -36,32 +36,51 @@
 // ****************************************************************
 
 // Map Method
-const angka = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-const angkaDouble = angka.map(function (num) {
-  return num * 2;
-});
+// const angka = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// const angkaDouble = angka.map(function (num) {
+//   return num * 2;
+// });
 
-const anime = [
+// const anime = [
 
-  {
-    title: 'Attack On Titan',
-    rating: '90',
-  },
-  {
-    title: 'One Piece',
-    rating: '89',
-  },
-  {
-    title: 'Bleach',
-    rating: '82',
-  },
-  {
-    title: 'Hunter X Hunter',
-    rating: '80',
-  },
+//   {
+//     title: 'Attack On Titan',
+//     rating: '90',
+//   },
+//   {
+//     title: 'One Piece',
+//     rating: '89',
+//   },
+//   {
+//     title: 'Bleach',
+//     rating: '82',
+//   },
+//   {
+//     title: 'Hunter X Hunter',
+//     rating: '80',
+//   },
 
-];
+// ];
 
-const animeList = anime.map(function (anime) {
-  return anime.title.toUpperCase();
-});
+// const animeList = anime.map(function (anime) {
+//   return anime.title.toUpperCase();
+// });
+
+// ***************************************************************
+
+// Sebelum Lanjut Kenalan Dengan Arrow Function
+
+// function perpangkatan(x) {
+//   return x * x;
+// }
+
+// const hasil = perpangkatan(5);
+
+// const perpangkatan = (x) => {
+//   return x * x;
+// };
+
+const random = () => {
+  return Math.floor(Math.random()) + 1;
+
+}
