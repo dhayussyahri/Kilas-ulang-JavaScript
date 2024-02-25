1fbf41d60130254256a7fb75e660e7c61e89d943
dhayus <74321000+dhayus@users.noreply.github.com>
2024-02-26 00:30:15 +0700

    25/02/24: Mengubah Array Atau Object Menjadi Deret Value Argument Function

--- app5.js
+++ app5.js
@@ -7,3 +7,11 @@ function LemparDadu(sisi = 6) {
 function sapa(name = 'user', msg = 'Selamat Bekerja') {
    console.log(`${name} ${msg}`);
 }
+
+// *******************************************************
+
+// Mengubah Array Atau Object Menjadi Deret Value Argument Function
+const angka = [1, 2, 3, 4, 5];
+
+// tambahkan "..." untuk mengubah array menjadi object
+Math.min(...angka);
