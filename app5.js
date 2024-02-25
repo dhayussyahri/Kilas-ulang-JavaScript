592fa4eb387555251e9be699353fa7fa18758e3d
dhayus <74321000+dhayus@users.noreply.github.com>
2024-02-26 00:47:56 +0700

    26/02/24:Menggabungkan Nilai Array Dengan Array Lagi

--- app5.js
+++ app5.js
@@ -1,17 +1,27 @@
 // Mengatur Default Value Pada Function
-function LemparDadu(sisi = 6) {
-   return Math.floor(Math.random() * sisi) + 1;
+// function LemparDadu(sisi = 6) {
+//    return Math.floor(Math.random() * sisi) + 1;
 
-}
+// }
 
-function sapa(name = 'user', msg = 'Selamat Bekerja') {
-   console.log(`${name} ${msg}`);
-}
+// function sapa(name = 'user', msg = 'Selamat Bekerja') {
+//    console.log(`${name} ${msg}`);
+// }
 
 // *******************************************************
 
 // Mengubah Array Atau Object Menjadi Deret Value Argument Function
-const angka = [1, 2, 3, 4, 5];
+// const angka = [1, 2, 3, 4, 5];
 
-// tambahkan "..." untuk mengubah array menjadi object
-Math.min(...angka);
+// // tambahkan "..." untuk mengubah array menjadi object
+// Math.min(...angka);
+
+// *****************************************************
+
+// Menggabungkan Nilai Array Dengan Array Lagi
+const angka = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+const nama = ['Dhayus', 'Hagi', 'Peta', 'Wili'];
+
+const campuran = [...angka, ...nama];
+
+// *****************************************************
